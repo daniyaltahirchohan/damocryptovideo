46ebb7a0296edd5e306cf41fdc31f4c6d1345e3d
dani_tahir360@hotmail.com <daniyaltahirchohan@hotmail.com>
2025-12-30 22:00:14 +0500

    six commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,22 @@
-//import { dirname } from "path";
-//import { fileURLToPath } from "url";
-//import { FlatCompat } from "@eslint/eslintrc";
-//
-//const __filename = fileURLToPath(import.meta.url);
-//const __dirname = dirname(__filename);
-//
-//const compat = new FlatCompat({
-//  baseDirectory: __dirname,
-//});
-//
-//const eslintConfig = [
-//  ...compat.extends("next/core-web-vitals", "next/typescript"),
-//];
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
 
-export default [
-  // Your existing config...
+const eslintConfig = defineConfig([
+  ...nextVitals,
   {
     rules: {
-      // Disable specific rules that are causing issues
-      "react/no-unescaped-entities": "off",
-      "@next/next/no-img-element": "off",
-      // Add other rules you need to disable
-    }
-  }
-]
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  },
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+])
 
-//export default eslintConfig;
+export default eslintConfig
